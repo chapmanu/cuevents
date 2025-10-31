c3c922c5a2b3a9b5b160463f184725901ab470bb
brfreeman <brfreeman@chapman.edu>
2025-10-31 14:04:18 -0700

    Create index.php
    Adding /fish calendar page

---
+++ fish/index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<?php include "../includes/header.php" ?>
+  
+      <div class="container-fluid bg-dark">
+        <div class="container pt-3 pb-3">
+          <div class="row">
+            <div class="col-md-6">
+              <h1 class="text-white"><a href="<?php echo $baseUrl.'/fish/index.php' ?>">Fish Interfaith Center Events </a></h1>
+            </div>
+            <div class="col-md-6">
+              <a class="btn btn-danger float-end border-0 rounded-0" href="<?php echo $basePath.'/submit-events.php' ?>"> Submit an Event </a>
+            </div>
+        </div>
+        </div>
+      </div>
+
+    
+      <div class="container">
+
+      <div class="row pt-4">
+           <div class="col-md-9">
+            <div id="months" class="mb-2 bg-sand p-3 pb-2"></div>
+          </div>
+          <div class="col-md-3">
+            <div id="search">
+            </div>
+          </div>
+        </div>
+
+        <div class="row pt-4">
+      
+        
+          <div class="col-md-9">
+      
+            
+            <div id="events"></div>
+          </div>
+          <div class="col-md-3">
+
+            <div id="dates" class="mt-4">
+            </div>
+
+
+            <?php include "../includes/calendar-links.php" ?>
+          </div>
+          
+        </div>
+      </div>
+      <?php include "../includes/footer.php" ?>
+    <script type="text/javascript">
+       $Trumba.addSpud({
+          webName: "fish-interfaith-center-calendar",
+          spudType : "searchlabeled",
+           spudId: "search"
+        });
+
+        $Trumba.addSpud({
+        webName: "fish-interfaith-center-calendar",
+        spudType : "main",
+        spudId: "events" });
+
+        $Trumba.addSpud({
+        webName: "fish-interfaith-center-calendar",
+        spudType : "datefinder" ,
+        teaserBase : "<?php echo $baseUrl.'/fish/index.php' ?>",
+        spudId: "dates" });
+
+        $Trumba.addSpud({
+        webName: "fish-interfaith-center-calendar",
+        spudType : "monthlist", 
+        spudId: "months"
+        });
+
+        $Trumba.addSpud({
+        webName: "fish-interfaith-center-calendar",
+        spudType : "filter",
+        spudId: "calendars" });
+       
+
+        $(document).ready(function() {
+          if ($('#ctl04_credit').length) {
+            $('#ctl04_credit').hide();
+          } else {
+            console.log("Element with ID 'yourID' is not loaded.");
+          }
+        });
+     
+    </script>
+    
+  </body>
+</html>
